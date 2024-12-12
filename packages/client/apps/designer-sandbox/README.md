d6683075d8cf0c00c6c83698132f8fa5c289e87f
Andre Torgal <hello@andretorgal.com>
2024-12-12 21:46:53 +0100

    create designer-sandbox app; configure some things for better mdx support

---
+++ packages/client/apps/designer-sandbox/README.md
@@ -0,0 +1,31 @@
+# UI System Sandbox
+
+> Sandbox application to develop, showcase and document UI System features.
+
+## Development
+
+### Getting started
+
+Make sure you [README](../../../../README.md) first.
+
+Launch the application in dev mode:
+
+```
+nvm use
+npm install
+npm run dev
+```
+
+### Stack
+
+- [Starlight](https://starlight.astro.build/) / [Astro](https://docs.astro.build)
+
+### Noodles
+
+## MIT License
+
+Copyright (c) 2024 [Noodlestan](https://noodlestan.org/).
+
+Published under a [MIT license](https://noodlestan.mit-license.org/).
+
+## 👀 Want to learn more?
--- packages/client/libs/ui-system/src/organisms/TreeList/components/TreeListNodeChildren/TreeListNodeChildren.tsx
+++ packages/client/libs/ui-system/src/organisms/TreeList/components/TreeListNodeChildren/TreeListNodeChildren.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'solid-js';
+import { Component, For } from 'solid-js';
 
 import { TreeNode, TreeNodeItemComponent, TreeState } from '../../types';
 import { TreeListNode } from '../TreeListNode';
@@ -24,17 +24,19 @@ export const TreeListNodeChildren: Component<TreeListNodeChildrenProps> = props 
 
     return (
         <div classList={classList()} style={style()}>
-            {props.children.map(child => (
-                <TreeListNode
-                    node={child}
-                    state={props.state}
-                    root={props.root}
-                    level={props.level + 1}
-                    parent={props.parent}
-                    isParentSelected={props.isParentSelected}
-                    component={props.component}
-                />
-            ))}
+            <For each={props.children}>
+                {child => (
+                    <TreeListNode
+                        node={child}
+                        state={props.state}
+                        root={props.root}
+                        level={props.level + 1}
+                        parent={props.parent}
+                        isParentSelected={props.isParentSelected}
+                        component={props.component}
+                    />
+                )}
+            </For>
         </div>
     );
 };
