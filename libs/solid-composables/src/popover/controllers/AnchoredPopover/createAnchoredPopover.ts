d3c7a055a0508f4fd199dde634509944cb2e3b22
Andre Torgal <hello@andretorgal.com>
2026-06-23 18:46:38 +0200

    extract function to new file

--- libs/solid-composables/src/popover/controllers/AnchoredPopover/createAnchoredPopover.ts
+++ libs/solid-composables/src/popover/controllers/AnchoredPopover/createAnchoredPopover.ts
@@ -6,37 +6,8 @@ import { createPlacement } from '../../../placement';
 import { createPopover } from '../Popover';
 
 import { $ANCHORED_POPOVER } from './constants';
-import type {
-	AnchoredPopoverAPI,
-	AnchoredPopoverProps,
-	AnchoredPopoverTriggerAPI,
-	AnchoredPopoverTriggerProps,
-} from './types';
-
-export const createAnchoredPopoverTrigger = (
-	props: AnchoredPopoverTriggerProps,
-): AnchoredPopoverTriggerAPI => {
-	const [locals, expose] = createExposable($ANCHORED_POPOVER, props);
-
-	const randomId = createMemo(shortId);
-	const id = () => locals.id ?? randomId();
-
-	const $static = {
-		popoverTargetAction: 'toggle',
-		'aria-haspopup': 'true',
-	} as const;
-
-	const $root = computedProps($static, {
-		id,
-		ref: () => locals.ref,
-		popoverTarget: () => locals.targetId,
-		'aria-expanded': () => locals.expanded,
-	});
-
-	return exposeAPI(expose, '$root', {
-		$root,
-	});
-};
+import { createAnchoredPopoverTrigger } from './createAnchoredPopoverTrigger';
+import type { AnchoredPopoverAPI, AnchoredPopoverProps } from './types';
 
 export const createAnchoredPopover = (props: AnchoredPopoverProps): AnchoredPopoverAPI => {
 	const [locals, expose, compose] = createExposable($ANCHORED_POPOVER, props);
