5d736521f5ec6bc455a11f724b3b4248124abaa2
Andre Torgal <hello@andretorgal.com>
2026-06-27 14:09:58 +0200

    delete unused code

--- cli/meta-extract/src/heuristics/helpers/resolveEntityPartial.ts
+++
@@ -1,9 +0,0 @@
-import type { EntityDataBasePartial } from '@purrception/primitives';
-import type { DirectoryExtractContext, EntityMetaMatcher } from '@purrception/source-fs';
-
-export async function resolveEntityPartial<P extends EntityDataBasePartial>(
-	ctx: DirectoryExtractContext,
-	matcher: EntityMetaMatcher<P>,
-): Promise<P | undefined> {
-	return await (matcher && matcher(ctx));
-}
--- cli/meta-extract/src/index.ts
+++ cli/meta-extract/src/index.ts
@@ -1,4 +1,3 @@
 // @index(['./*.{ts,tsx}', './!(private|parts|functions)*/index.{ts,tsx}'], f => `export * from '${f.path.replace(/\/index$/, '')}';`)
 export * from './entities';
-export * from './heuristics';
 export * from './utils';
