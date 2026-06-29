5053d73b2e843470e4509fd0247720947cdf1133
Andre Torgal <hello@andretorgal.com>
2026-06-29 22:29:10 +0200

    align NavLink sizes and typopgrahy with size scale and shorten animations

--- libs/standard-ui/src/navigation/mixins/NavLink/createNavLinkMixin.ts
+++ libs/standard-ui/src/navigation/mixins/NavLink/createNavLinkMixin.ts
@@ -6,6 +6,7 @@ import {
 	createClassList,
 } from '@no-comply/solid-primitives';
 
+import { createActionLabelMixin } from '../../../typography';
 import { createLinkMixin } from '../Link';
 
 import styles from './NavLinkMixin.module.scss';
@@ -21,11 +22,14 @@ const defaultProps: PickRequired<NavLinkMixinProps, 'layout' | 'size' | 'highlig
 export const createNavLinkMixin = (props: NavLinkMixinProps): NavLinkMixinAPI => {
 	const [locals, expose, compose] = createExposable($NAV_LINK_MIXIN, props);
 
-	const { $root: $linkMixinRoot } = compose(createLinkMixin(locals));
-
 	const layout = () => locals.layout ?? defaultProps.layout;
 	const size = () => locals.size ?? defaultProps.size;
 	const highlight = () => locals.highlight ?? defaultProps.highlight;
+
+	const { $root: $linkMixinRoot } = compose(createLinkMixin(locals));
+
+	const actionLabelMixinProps = computedProps({ variant: size });
+	const { $root: $actionLabelMixinRoot } = compose(createActionLabelMixin(actionLabelMixinProps));
 	const classList = createClassList(styles, () => ({
 		NavLink: true,
 		[`layout-${layout()}`]: true,
@@ -39,6 +43,6 @@ export const createNavLinkMixin = (props: NavLinkMixinProps): NavLinkMixinAPI =>
 	});
 
 	return exposeAPI(expose, '$root', {
-		$root: combineProps($linkMixinRoot, $root),
+		$root: combineProps($linkMixinRoot, $actionLabelMixinRoot, $root),
 	});
 };
