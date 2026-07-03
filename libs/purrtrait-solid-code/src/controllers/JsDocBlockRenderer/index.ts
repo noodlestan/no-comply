b98bd39136f87ff7724d3b1401f7e959251b2d79
Andre Torgal <hello@andretorgal.com>
2026-07-03 17:15:26 +0200

    add renderer controllers

--- libs/purrtrait-solid-code/src/controllers/JsDocBlockRenderer/index.ts
+++ libs/purrtrait-solid-code/src/controllers/JsDocBlockRenderer/index.ts
@@ -1,3 +1,3 @@
 // @index(['./*.{ts,tsx}', './!(private|parts|functions)*/index.{ts,tsx}'], f => `export * from '${f.path.replace(/\/index$/, '')}';`)
-export * from './JsDocBlockRenderer';
-export * from './JsDocLinkResolver';
+export * from './createJsDocBlockRenderer';
+export * from './types';
